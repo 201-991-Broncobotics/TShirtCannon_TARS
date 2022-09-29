cf8650a40e542e8a5c55fd400091a9e01db68827
NGeorge25 <ngeorge23@brophybroncos.org>
2022-09-29 10:07:40 -0700

    Air Horn Added

---
+++ TShirt Cannon 2022-2023/src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,142 @@
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+//import edu.wpi.first.wpilibj.smartdashboard.*;
+//import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.Joystick;
+
+//import edu.wpi.first.wpilibj.DriverStation;
+//import com.ctre.phoenix.motorcontrol.en
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+
+//@Override
+public class Drivetrain
+{
+    // defines the motors and other sensors
+    TalonSRX frontLeft;
+    TalonSRX frontRight;
+    TalonSRX backLeft;
+    TalonSRX backRight;
+    BuiltInAccelerometer gyro = new BuiltInAccelerometer();
+    boolean strafing = true;
+    double gyro_angle_ref = 0;
+
+    public int dividyNum = 1;
+
+    //SmartDashboard my_dash = new SmartDashboard();
+
+    public Drivetrain(int __frontLeft__, int __frontRight__, int __backLeft__, int __backRight__)
+    {
+        // defines names for the motors
+        frontLeft = new TalonSRX(__frontLeft__);
+        frontRight = new TalonSRX(__frontRight__);
+        backLeft =  new TalonSRX(__backLeft__);
+        backRight =  new TalonSRX(__backRight__);
+
+        // reverses motors so that you ovefoward with positive values
+        frontLeft.setInverted(true);
+        backLeft.setInverted(true);
+        backRight.setInverted(true);
+
+    }
+
+    public void MoveRobot(float leftStickX, float leftStickY, float rightStickX, boolean deadZoneOff)
+    {
+        // inverts the left stick y value
+        leftStickY *= -1;
+
+        // stops the motors when stick not pressed
+        backLeft.set(ControlMode.PercentOutput, 0);
+        backRight.set(ControlMode.PercentOutput, 0);
+        frontLeft.set(ControlMode.PercentOutput, 0);
+        frontRight.set(ControlMode.PercentOutput, 0);
+
+        // which ever stick is pushed farthest is the one that it does
+        if (Math.abs(leftStickX) > .2f || Math.abs(rightStickX) > .2f || Math.abs(leftStickY) > .2f || deadZoneOff)
+        {
+            if (Math.abs(leftStickX) > Math.abs(leftStickY) && Math.abs(leftStickX) > Math.abs(rightStickX))
+            {
+                
+                
+              /*if (strafing = false)
+                {
+                   DriverStation.reportError(String.valueOf(gyro.getY()), false);
+                   //SmartDashboard.putNumber(("y_val"), gyro.getY());
+                   
+                }
+                double leftStickX_l = rightStickX + (gyro.getY());
+                if (leftStickX_l > 1)
+                {
+                    leftStickX_l = 1;
+                }
+                else if (leftStickX_l < -1)
+                {
+                    leftStickX = -1;
+                }*/
+                //strafing = true;
+                // strafes
+                backLeft.set(ControlMode.PercentOutput, leftStickX/dividyNum);
+                backRight.set(ControlMode.PercentOutput, -leftStickX/dividyNum);
+                frontLeft.set(ControlMode.PercentOutput, -leftStickX/dividyNum);
+                frontRight.set(ControlMode.PercentOutput, leftStickX/dividyNum);
+            }
+
+            else if (Math.abs(leftStickY) > Math.abs(leftStickX) && Math.abs(leftStickY) > Math.abs(rightStickX))
+            {
+                strafing = false;
+
+                // drives fowards and backwards
+                backLeft.set(ControlMode.PercentOutput, leftStickY/dividyNum);
+                backRight.set(ControlMode.PercentOutput, leftStickY/dividyNum);
+                frontLeft.set(ControlMode.PercentOutput, leftStickY/dividyNum);
+                frontRight.set(ControlMode.PercentOutput, leftStickY/dividyNum);
+            }
+
+            else
+            {
+                strafing = false;
+                // turns the robo
+                backLeft.set(ControlMode.PercentOutput, -rightStickX/dividyNum);
+                backRight.set(ControlMode.PercentOutput, rightStickX/dividyNum);
+                frontLeft.set(ControlMode.PercentOutput, -rightStickX/dividyNum);
+                frontRight.set(ControlMode.PercentOutput, rightStickX/dividyNum);
+            }
+        }
+
+        
+
+    }
+
+    public void drive(Joystick gamepad){
+        double lx = deadzone(gamepad.getRawAxis(0));
+        double ly = -deadzone(gamepad.getRawAxis(1));
+        double rx = deadzone(gamepad.getRawAxis(4));
+
+        double x1 = Math.abs(lx);
+        double y1 = Math.abs(ly);
+        double x2 = Math.abs(rx);
+
+        double bL = ((y1 > x1 && y1 > x2) ? ly : (x1 > x2) ? -lx : rx);
+        double bR = ((y1 > x1 && y1 > x2) ? ly : (x1 > x2) ? lx : -rx);
+        double fR = ((y1 > x1 && y1 > x2) ? ly : (x1 > x2) ? -lx : -rx);
+        double fL = ((y1 > x1 && y1 > x2) ? ly : (x1 > x2) ? lx : rx);
+
+        System.out.println(bL + " " + bR + " " + fL + " " + fR);
+
+        backLeft.set(ControlMode.PercentOutput, bL);
+        backRight.set(ControlMode.PercentOutput, bR);
+        frontLeft.set(ControlMode.PercentOutput, fL);
+        frontRight.set(ControlMode.PercentOutput, fR);
+
+
+    }
+    
+    public double deadzone(double in){
+        return Math.abs(in) > 0.1 ? in : 0;
+    }
+    
+}
---
+++ TShirt Cannon 2022-2023/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,77 @@
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+
+public class Robot extends TimedRobot {
+  private final Joystick m_stick = new Joystick(0);
+
+  // Solenoid corresponds to a single solenoid.
+  private final Solenoid m_solenoid_shoot = new Solenoid(PneumaticsModuleType.CTREPCM, 0);
+  private final Solenoid m_solenoid_load = new Solenoid(PneumaticsModuleType.CTREPCM, 2);
+  private final Solenoid m_solenoid_horn = new Solenoid(PneumaticsModuleType.CTREPCM, 4);
+  private TalonSRX shooter = new TalonSRX(0);
+  
+  // DoubleSolenoid corresponds to a double solenoid.
+  //private final DoubleSolenoid m_doubleSolenoid = new DoubleSolenoid(1, 2);
+
+  private static final int kSolenoidShootButton = 0;
+  private static final int kSolenoidLoadButton = 2;
+
+  private Drivetrain dT;
+
+  @Override
+  public void robotInit() {
+    /*UsbCamera camera = CameraServer.startAutomaticCapture();
+   
+    camera.setResolution(1024, 1024);
+    camera.setFPS(30);*/
+    dT = new Drivetrain(3, 8, 6, 4);
+
+  }
+
+
+  @Override
+  public void teleopPeriodic() 
+  { 
+    
+   dT.drive(m_stick);
+    if(m_stick.getRawAxis(3)>0.1)
+    {
+      m_solenoid_shoot.set(true);
+      m_solenoid_load.set(false);
+    }
+    else
+    {
+      m_solenoid_shoot.set(false);
+      m_solenoid_load.set(true);
+    }
+    //m_solenoid_shoot.set(true);
+  
+    if(m_stick.getRawButton(5)){
+        shooter.set(ControlMode.PercentOutput, 0.2);
+    }else if(m_stick.getRawButton(6)){
+      shooter.set(ControlMode.PercentOutput, -0.2);
+    }else{
+      shooter.set(ControlMode.PercentOutput, 0);
+    }
+
+    if(m_stick.getRawButton(4) && m_stick.getRawAxis(2) > 0.5 && m_stick.getRawButton(2)){
+        m_solenoid_horn.set(true);
+    }else{
+        m_solenoid_horn.set(false);
+    }
+
+  }
+  
+}
